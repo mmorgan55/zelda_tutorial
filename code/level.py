856dfae4ceb128698b997f2b10672f1caefe20eb
M5 <pureanarchy.mm@gmail.com>
2022-06-12 13:59:46 -0600

    Implemented basic movement for the player ( up, down, left, right).

--- code/level.py
+++ code/level.py
@@ -1,4 +1,8 @@
 import pygame as pg
+from settings import *
+from tile import Tile
+from player import Player
+from debug import debug
 
 
 class Level:
@@ -10,5 +14,22 @@ class Level:
         self.visible_sprites = pg.sprite.Group()
         self.obstacle_sprites = pg.sprite.Group()
 
+        self.player = None
+
+        # Sprite setup
+        self.create_map()
+
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILE_SIZE
+                y = row_index * TILE_SIZE
+                if col == 'x':
+                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+                if col == 'p':
+                    self.player = Player((x, y), [self.visible_sprites])
+
     def run(self):
-        pass
+        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
--- code/tile.py
+++ code/tile.py
@@ -1,9 +1,12 @@
 import pygame as pg
 from settings import *
+from os.path import dirname, join
+
+dir_name = dirname(__file__)
 
 
 class Tile(pg.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pg.image.load('/graphics/test/rock.png').convert_alpha()
+        self.image = pg.image.load(join(dir_name, '../graphics/test/rock.png')).convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
