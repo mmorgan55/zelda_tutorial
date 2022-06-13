435323235c8426d9624e9f55856a513d4e8ef643
M5 <pureanarchy.mm@gmail.com>
2022-06-13 13:38:40 -0600

    Added graphics for the ground. Implemented a function to draw the map layout from a csv files and added a boundary to the map.

--- code/level.py
+++ code/level.py
@@ -3,6 +3,10 @@ from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
+from os.path import dirname, join
+from support import import_csv_layout
+
+dir_name = dirname(__file__)
 
 
 class Level:
@@ -20,14 +24,18 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILE_SIZE
-                y = row_index * TILE_SIZE
-                if col == 'x':
-                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
-                if col == 'p':
-                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
+        layouts = {
+            'boundary': import_csv_layout(join(dir_name, '../map/map_FloorBlocks.csv'))
+        }
+        for style, layout in layouts.items():
+            for row_index, row in enumerate(layout):
+                for col_index, col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILE_SIZE
+                        y = row_index * TILE_SIZE
+                        if style == 'boundary':
+                            Tile((x, y), [self.obstacle_sprites], 'invisible')
+        self.player = Player((2000, 1500), [self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
         self.visible_sprites.custom_draw(self.player)
@@ -44,10 +52,18 @@ class YSortCameraGroup(pg.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] / 2
         self.offset = pg.math.Vector2(100, 200)
 
+        # Creating the floor
+        self.floor_surf = pg.image.load(join(dir_name, '../graphics/tilemap/ground.png')).convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft=(0, 0))
+
     def custom_draw(self, player):
         # Getting offset for the player camera
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        # Drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
 
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
--- code/tile.py
+++ code/tile.py
@@ -6,8 +6,9 @@ dir_name = dirname(__file__)
 
 
 class Tile(pg.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, sprite_type, surface=pg.Surface((TILE_SIZE, TILE_SIZE))):
         super().__init__(groups)
-        self.image = pg.image.load(join(dir_name, '../graphics/test/rock.png')).convert_alpha()
+        self.sprite_type = sprite_type
+        self.image = surface
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -10)
