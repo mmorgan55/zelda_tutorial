05fa2eddfaf1b041cdc28fcb22b55b46960ab418
M5 <pureanarchy.mm@gmail.com>
2022-06-13 15:38:43 -0600

    Added base logic for attacking and casting magic.

--- code/player.py
+++ code/player.py
@@ -11,14 +11,19 @@ class Player(pg.sprite.Sprite):
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -26)
 
+        # Player actions
         self.direction = pg.math.Vector2()
         self.speed = 5
+        self.attacking = False
+        self.attack_cooldown = 400
+        self.attack_time = None
 
         self.obstacles = obstacles
 
     def input(self):
         keys = pg.key.get_pressed()
 
+        # Movement input
         if keys[pg.K_UP]:
             self.direction.y = -1
         elif keys[pg.K_DOWN]:
@@ -32,6 +37,16 @@ class Player(pg.sprite.Sprite):
             self.direction.x = 1
         else:
             self.direction.x = 0
+
+        # Attack input
+        if keys[pg.K_SPACE] and not self.attacking:
+            self.attacking = True
+            self.attack_time = pg.time.get_ticks()
+
+        # Magic input
+        if keys[pg.K_LSHIFT] and not self.attacking:
+            self.attacking = True
+            self.attack_time = pg.time.get_ticks()
 
     def move(self, speed):
         if self.direction.magnitude() != 0:
@@ -60,6 +75,14 @@ class Player(pg.sprite.Sprite):
                     if self.direction.y < 0:  # Moving  up
                         self.hitbox.top = sprite.hitbox.bottom
 
+    def cooldowns(self):
+        current_time = pg.time.get_ticks()
+
+        if self.attacking:
+            if current_time - self.attack_time >= self.attack_cooldown:
+                self.attacking = False
+
     def update(self):
         self.input()
+        self.cooldowns()
         self.move(self.speed)
